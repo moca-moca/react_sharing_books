f94268b54431f6046992907323a7b4db9ef47e77
Shimozono Misato <>
2018-06-30 20:03:00 +0900

    カードタイプで表示

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
-const Book = () => {
-    return (
-        <div>Book</div>
-    );
+const styles = {
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%'
+  }
 }
 
-export default Book;
+const Book = (props) => {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={props.book.imageLinks.smallThumbnail}
+          title="book image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="headline" component="h2">
+            {props.book.title}
+          </Typography>
+          <Typography component="p">
+            {props.book.description}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+Book.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Book);
