42fdcc836d0d53b5d012e91aec85e35f78d43637
Shimozono Misato <>
2018-06-30 20:13:23 +0900

    1行4列でで表示

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Book from './Book';
 
-class Bookshelf extends Component {
-    renderList() {
-        return this.props.books.map((book) => {
-            return (
-                <li key={book.title}>
-                    <Book book={book} />
-                </li>
-            );
-        })
-    }
-
-    render() {
-        return (
-            <ul>
-                {this.renderList()}
-            </ul>
-        );
-    }
+const styles = {
+  root: {
+    flexGrow: 1,
+    padding: '50px'
+  }
 }
 
-export default Bookshelf;
+class Bookshelf extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <Grid key={book.title} item xs={3}>
+          <Book book={book} />
+        </Grid>
+      );
+    })
+  }
+  
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Grid container className={classes.root} direction="row" justify="flex-start" spacing={40}>
+        {this.renderList()}
+      </Grid>
+    );
+  }
+}
+
+Bookshelf.PropTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Bookshelf);
